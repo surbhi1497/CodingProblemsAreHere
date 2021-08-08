dfebb640c61294dc6e9f874c3d5e1ffe404661c3
surbhi_sharma <surbhi_sharma@epam.com>
2021-08-08 23:47:54 +0530

    Top Questions in Array

---
+++ src/Arrays/TopQuestions/SubArrayWithZeroSum.java
@@ -0,0 +1,53 @@
+package Arrays.TopQuestions;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class SubArrayWithZeroSum {
+        public static void main (String[] args) {
+            //code
+            //code
+
+            //taking input using class Scanner
+            Scanner scan = new Scanner(System.in);
+
+            //taking total number of testcases
+            int t = scan.nextInt();
+            while(t>0)
+            {
+                //taking total count of elements
+                int n = scan.nextInt() ;
+
+                //Declaring and Initializing an array of size n
+                int arr[] = new int[n];
+
+                //adding elements to the array
+                for(int i = 0; i<n;i++)
+                {
+                    arr[i] = scan.nextInt();
+                }
+
+                t--;
+
+                //calling the method findSum
+                //and print "YES" or "NO" based on the result
+                System.out.println(new SubArrayWithZeroSum().findsum(arr,n)==true?"Yes":"No");
+            }
+        }
+
+
+        static boolean findsum(int arr[],int n)
+        {
+            int sum=0;
+            HashSet<Integer> hs = new HashSet<Integer>();
+            for(int i=0;i<n;i++){
+                sum+=arr[i];
+                if(arr[i]==0 || sum==0 || hs.contains(sum))
+                    return true;
+                hs.add(sum);
+            }
+            return false;
+
+            //Your code here
+        }
+
+}
